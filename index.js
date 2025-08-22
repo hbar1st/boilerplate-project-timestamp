0446681ab29d687fd92c9f5eb3b095668309f199
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-22 18:57:12 -0400

    attempt to print out required date formats

--- index.js
+++ index.js
@@ -18,12 +18,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+/**
+ * 
+ * A request to /api/:date? with a valid date should return a JSON object 
+ * with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
+ */
+app.get("/api/:date", (req, res) => {
+  const { date } = req.params;
+  const now = new Date().toString();
+  let dateResponse = {
+    unix: now,
+    utc: now,
+  };
+  if (date !== "") {
+    const dateObj = new Date(date);
+    if (isNaN(dateObj)) {
+      dateResponse = { error: "Invalid Date" };
+    } else {
+      dateResponse = { unix: "good Date", utc: "good time" };
+    }
+    
+  }
+  res.json(dateResponse);
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
